7827ed28d4cbcd813fe93828e8a74a7daeaa31e2
Adam Alilou <aa1320@ic.ac.uk>
2023-02-27 14:31:11 +0000

    elixir version change

--- basicpaxos/mix.exs
+++ basicpaxos/mix.exs
@@ -5,7 +5,7 @@ defmodule MultiPaxos.MixProject do
     [
       app: :multi_paxos,
       version: "0.1.0",
-      elixir: "~> 1.14",
+      elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
